c93e225fbb6c096479945701162da91fc73a104a
Ramy <rami.naccache1@gmail.com>
2022-11-23 00:03:31 +0200

    code cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,12 @@ import {
   Route,
 } from "react-router-dom";
 import Home from "./pages/home/Home";
-// import Hotel from "./pages/hotel/Hotel";
-import List from "./pages/list/List";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/list" element={<List/>}/>
-        {/* <Route path="/hotels/:id" element={<Hotel/>}/> */}
       </Routes>
     </BrowserRouter>
   );
